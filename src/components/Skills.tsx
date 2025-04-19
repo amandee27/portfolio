5768631ba402577738435799edcc7effe70271f5
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-04-19 12:19:24 +0100

    Reformat the code by reusing same component for repeating items

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,131 +1,45 @@
 import React from "react";
 
+const skills = [
+  "React",
+  "Angular",
+  "Typescript",
+  "Javascript",
+  "HTML",
+  "CSS",
+  "SCSS",
+  "AntD",
+  "Tailwind CSS",
+  "BootStrap",
+  "NodeJS",
+  "MySQL",
+  "Java",
+  "NPM",
+  "Selenium",
+  "Responsive Design",
+  "Cross-Browser Compatibility",
+  "Agile Development",
+  "Scrum",
+];
+
 function Skills() {
   return (
-    <div className="w-full justify-center">
-      <h1 className="text-center text-5xl my-12">Skills</h1>
-      <div className="grid grid-cols-5 ">
-        <div></div>
-        <div className="col-span-3">
+    <div className="w-full justify-center bg-slate-800/60 py-12">
+      <h1 className="text-center text-5xl mb-12 font-bold">Skills</h1>
+      <div className="justify-items-center">
+        <div className="w-3xl">
           <div className="flex flex-wrap justify-center align-middle">
-            <button
-              type="button"
-              className="bg-black hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              React
-            </button>
-            <button
-              type="button"
-              className="bg-black hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Angular
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Typescript
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Javascript
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              HTML
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              CSS
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              SCSS
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              AntD
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Tailwind CSS
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              BootStrap
-            </button>
-            <button
-              type="button"
-              className="bg-black hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              NodeJS
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              MySQL
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Java
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              NPM
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Selenium
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Responsive Design
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Cross-Browser Compatibility
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Agile Development
-            </button>
-            <button
-              type="button"
-              className="bg-black  hover:bg-black border border-white text-white text-[14px] rounded-full px-3 mx-2 my-1"
-            >
-              Scrum
-            </button>
+            {skills.map((item) => (
+              <button
+                type="button"
+                key={item}
+                className="bg-black hover:bg-black border border-white text-white text-[16px] rounded px-3 py-1 mx-2 my-1"
+              >
+                {item}
+              </button>
+            ))}
           </div>
         </div>
-
-        <div></div>
       </div>
     </div>
   );
