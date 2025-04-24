aee012b8f9aa9797282ca99557cc2c56c99dcd74
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-04-24 14:56:02 +0100

    Add back button to workout tracker page

--- src/app/projects/workout-tracker/page.tsx
+++ src/app/projects/workout-tracker/page.tsx
@@ -14,34 +14,35 @@ function WorkoutTracker() {
   const router = useRouter();
   return (
     <div>
-      <div className="flex flex-wrap justify-end my-6">
-        <button
-          type="button"
-          className="text-white  hover:bg-gray-400 rounded focus:ring-1  font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center   me-2 mb-2"
-          onClick={() => router.push("/")}
-        >
-          <svg
-            className="w-6 h-6 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m15 19-7-7 7-7"
-            />
-          </svg>
-          Back
-        </button>
-      </div>
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 bg-gray-700/20 min-h-screen">
-        <h1 className="font-bold text-5xl my-6">Workout Tracker</h1>
+        <div className="flex justify-start">
+          <button
+            type="button"
+            className="text-white hover:underline font-medium  text-sm px-5 text-center inline-flex items-center"
+            onClick={() => router.push("/")}
+          >
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m15 19-7-7 7-7"
+              />
+            </svg>
+            Back
+          </button>
+          <h1 className="font-bold text-5xl my-6">Workout Tracker</h1>
+        </div>
+
         <h2 className="font-bold text-3xl my-4">Tech Stack</h2>
         <div className="flex flex-wrap">
           <button
