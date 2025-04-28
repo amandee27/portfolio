91e9cb745215dee9a51133c88562f47b12114144
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-04-28 17:35:34 +0100

    Remove deployment links from home page projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -46,32 +46,6 @@ function Projects() {
                   </h5>
                 </a>
               </div>
-              <div>
-                <a
-                  href={item.demo}
-                  className="inline-flex items-center mt-1  text-sm font-medium text-center text-white focus:outline-none  "
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <svg
-                    className="w-6 h-6 text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"
-                    />
-                  </svg>
-                </a>
-              </div>
             </div>
 
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
