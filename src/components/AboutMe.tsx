63cb5012711ebaa59e1f582702d11e7309b28aaf
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-05-06 12:48:37 +0100

    Profile data update

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import profileImage from "../shared/ProfileImage3.jpg";
+import profileImage from "../shared/Profile.jpg";
 
 function AboutMe() {
   return (
